cb03f2696b38547e01b76669fc1b5fa68048c547
woong <ccw1021@naver.com>
2018-04-06 17:21:35 +0900

    create appPage, designPage, introductionPag

---
+++ src/Design.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import Sample from './designSample.jpg';
+import './Design.css';
+
+
+class Design extends Component {
+  render() {
+    return (
+      <div className="Design">
+        {/* 디자인 이미지 */}
+        <div className="Design__Colums">
+            <DesignImage />
+        </div>
+        {/* 디자인 소개 */}
+        <div className="Design__Colums">
+          <h1>Design sample</h1>
+          <p className="Design__Synopsis"> 
+              asdgasdasfsdf
+              asdfasdf
+              asdfasdfasdf
+              asdfasdf
+          </p>
+        </div>
+      </div>
+    )
+  }
+}
+
+class DesignImage extends Component {
+
+  render() {
+    return (
+      // <img src={design} />
+      // <img src={asset} alt="designSample.jpg" />
+      <img src={Sample} className="Design-image" alt="sample" className="Design__Image"/>
+    );
+  }
+}
+
+export default Design;
---
+++ src/Introduction.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+
+class Introduction extends Component {
+  render() {
+    return (
+      <div className="Introduction">
+        <div className="Introduction__Colums">
+          <h1>Introduct component</h1>
+          <p>
+            You can show fantistic magic
+            This is introduction for our project
+            com
+            com
+            made by react
+            beta
+            OOOO
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Introduction;
