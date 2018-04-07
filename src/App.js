4bb075b1c6bbd7eeb11cd4aeea5c0e04eb579403
ieon <kimiron518@naver.com>
2018-04-07 23:35:46 +0900

    add link from App to Register & add home page

--- src/App.js
+++ src/App.js
@@ -2,22 +2,39 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Design from './Design';
+
+import {Router, Switch, Route, Link} from 'react-router-dom';
+
+import Register from './components/Register';
+import Home from './components/Home';
 import Introduction from './Introduction';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to AR FUNiture</h1>
+          <span>
+            <button><Link to="/components/Register">Register</Link></button>
+          </span>
+          
         </header>
-        <Introduction />
-        <div className="App-design">      
-          <Design />
-          <Design />
-          <Design />
-          <Design />
+
+
+        <div className="body">
+          <Switch>
+            <Route path="/components/Home" exact component={Home} />
+            <Route path="/components/Register" component={Register} />
+            <Route path="/Introduction" component={Introduction}/>
+          </Switch>
+            {/* <Introduction /> */}
+            <div className="App-design">      
+              <Design />
+              <Design />
+              <Design />
+              <Design />
+            </div>
         </div>
       </div>
     );
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,1 +1,13 @@
 import React, { Component } from 'react'
+
+
+export default class Register extends Component{
+
+    render(){
+        return(
+            <div className="col-sm-6 col-sm-offset-3">
+                <h1>Register</h1>
+            </div>
+        )
+    }
+}
