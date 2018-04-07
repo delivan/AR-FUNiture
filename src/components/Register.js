358e77fe74bcc384f6b9000147ee4174e4091701
ieon <kimiron518@naver.com>
2018-04-07 23:57:14 +0900

    add form for input in register page

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,10 +3,25 @@ import React, { Component } from 'react'
 
 export default class Register extends Component{
 
+    handleSubmit = (e) => {
+        console.log(e);
+    }
+
+
     render(){
         return(
             <div className="col-sm-6 col-sm-offset-3">
                 <h1>Register</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <input className="form-control" onClick={this.handleSubmit} placeholder="Email"/>
+                    </div>
+                    <div className="form-group">
+                        <input type="password" className="form-control" placeholder="Password" />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Register</button>
+                    <button className="">Cancel</button>
+                </form>
             </div>
         )
     }
