e9ef060d82ffdc00e13e54cceeb924c5b70941d5
ieon <kimiron518@naver.com>
2018-04-09 00:53:43 +0900

    add register function & add users in firebase

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,10 +1,32 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import { register } from '../action/Auth';
+import { Link } from 'react-router-dom';
+import Home from './Home';
 
 export default class Register extends Component{
 
+
+    constructor(props){
+        super(props);
+        this.state = {
+            email: '',
+            pw: '',
+            phone: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        let user = {};
+        user[e.target.name] = e.target.value;
+        this.setState(user)
+
+    }
+
     handleSubmit = (e) => {
-        console.log(e);
+        register(this.state.email, this.state.pw, this.state.phone);    
     }
 
 
@@ -14,13 +36,15 @@ export default class Register extends Component{
                 <h1>Register</h1>
                 <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
-                        <input className="form-control" onClick={this.handleSubmit} placeholder="Email"/>
+                        <input className="form-control" name="email" value={this.state.email} onChange={this.handleChange} placeholder="Email"/>
                     </div>
                     <div className="form-group">
-                        <input type="password" className="form-control" placeholder="Password" />
+                        <input type="password" className="form-control" name="pw" value={this.state.pw} onChange={this.handleChange} placeholder="Password" />
+                    </div>
+                    <div className="form-group">
+                        <input type="phone" className="form-control" name="phone" value={this.state.phone} onChange={this.handleChange} placeholder="phone" />
                     </div>
                     <button type="submit" className="btn btn-primary">Register</button>
-                    <button className="">Cancel</button>
                 </form>
             </div>
         )
