a527b4742a30cb5df4f66c9d7b7f7e819dbbbf36
hyuk22 <wjdgurdl272@gmail.com>
2018-04-09 21:14:43 +0900

    add material-ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
-import logo from '../img/logo.svg';
-import '../css/App.css';
-import Design from './Design';
-
 import {Router, Switch, Route, Link} from 'react-router-dom';
-
-import Register from './Register';
 import Home from './Home';
+import Design from './Design';
+import Register from './Register';
 import Introduction from './Introduction';
 import Ar from './Ar';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import '../css/App.css';
 
+const styles = {
+    container: {
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        fontFamily: "'Roboto', sans-serif",
+    },
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <MuiThemeProvider>
+
+      <div style={styles.container}>
         <header className="App-header">
           <span>
             <button><Link to="/">Home</Link></button>
@@ -31,6 +41,7 @@ class App extends Component {
           </Switch>
         </div>
       </div>
+    </MuiThemeProvider>
     );
   }
 }
