7c9a9e9dd097ba3dc1c0b6e0915c3af9ec74b67d
ieon <kimiron518@naver.com>
2018-05-28 16:55:19 +0900

    Change the description of English to Hangul

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -39,13 +39,13 @@ class Introduction extends Component {
           <Media>
             <Media body>
               <Media heading>STEP 1</Media>
-              Place the Hiro mark in the desired position. Please select the design card below.
+              하단에서 마음에 드는 인테리어 디자인 카드를 선택해주세요. 가구를 놓고 싶은 자리에 Hiro 마크를 놓아두세요.
             </Media>
           </Media>
           <Media>
             <Media body>
               <Media heading>STEP 2</Media>
-                Keep the camera screen on the Hiro mark after the camera is turned on.
+                Hiro 마크를 둔 곳에 카메라를 올려두면 가구가 생성됩니다. 양 끝 화살표를 통해서 다양한 가구를 볼 수 있습니다.
             </Media>
           </Media>      
         </div>
