39ed95b4de1308a917cd3bad5da5cea6543fe365
ieon <kimiron518@naver.com>
2018-06-03 21:28:33 +0900

    Add margin of image.

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -24,6 +24,9 @@ class Introduction extends Component {
     //   }
     // }
     //gUwieWcISDU
+    const divStyle = {
+      margin: 30,
+    };
     return (
       <div className="Introduction" >
         
@@ -34,8 +37,8 @@ class Introduction extends Component {
           <Media>
             <Media body>
               <Media heading>STEP 1</Media>
-              <img src={homepage} alt="" width="300"/>
-              <span>       </span>
+              <img src={homepage} alt="" width="300" style={divStyle}/>
+              
               <img src={hiromark} alt="" width="300"/>
               <p>하단에서 마음에 드는 인테리어 디자인 카드를 선택해주세요. 가구를 놓고 싶은 자리에 Hiro 마크를 놓아두세요.</p>
             </Media>
@@ -44,8 +47,8 @@ class Introduction extends Component {
           <Media>
             <Media body>
               <Media heading>STEP 2</Media>
-              <img src={rendering1} alt="" width="300"/>
-              <span>       </span>
+              <img src={rendering1} alt="" width="300" style={divStyle}/>
+              
               <img src={rendering2} alt="" width="300"/>
               <p>Hiro 마크를 둔 곳에 카메라를 올려두면 가구가 생성됩니다. 양 끝 화살표를 통해서 다양한 가구를 볼 수 있습니다.</p>
             </Media>
